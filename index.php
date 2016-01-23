38c9a20f0afd7b4422397e37a08f50d803f6a181
kevinp89 <kevinptl4@gmail.com>
2016-01-23 10:21:17 -0500

    KEVIN: added new interface

--- index.php
+++ index.php
@@ -3,24 +3,76 @@
 		
 		<title> Calorie Detector </title>
 		<link rel="stylesheet" type="text/css" href="main.css"> 
+		<link rel="stylesheet" type="text/css" href="normalize.css"> 
 		
 	</head>
 	<body>
 		<div class="nav-container">
 			<nav>
-				<ul id="nav-lst">
-					<li><a href="index.php"><span>Home</span></a></li>
-					<li><a href="#"><span>Contact Us</span></a></li>			
-				</ul>
+			<ul>
+				<li><a href="index.php"> Home </a></li>
+				<li><a href="#1"> About Us </a></li>
+				<li><a href="#2"> Search </a></li>
+				<li><a href="#3"> News </a></li>
 				
-				
-				
+			</ul>
 			</nav>
+			
+		</div>
+
+<section class="panel b-blue" id="1">
+    <div class="panel__content">
+      <h1 class="panel__headline" id="aboutUs"><i class="fa fa-camera-retro"></i>About Us</h1>
+       	<ul>
+	      	<li>Kevin Patel</li>
+	      		<li>Email: <a href="#">kp.patel@mail.utoronto.ca</a></li>
+      	</ul>
+      
+      
+    </div>
+  </article>
+</section>
+
+
+
+<section class="panel b-yellow" id="2">
+  
+    <div class="panel__content" id="about-content">
+      <h1 class="panel__headline"><i class="fa fa-bolt"></i>&nbsp;Search</h1>
+      <div class="panel__block"></div>
+      
+      
+        </div>
+  
+  
+  
+  
+  
+</section>
+<section class="panel b-red" id="3">
+  <article class="panel__wrapper">
+    <div class="panel__content">
+      <h1 class="panel__headline"><i class="fa fa-music"></i>&nbsp;Music</h1>
+      <div class="panel__block"></div>
+      <p>Beard sriracha kitsch literally, taxidermy normcore aesthetic wayfarers salvia keffiyeh farm-to-table sartorial gluten-free mlkshk. Selvage normcore 3 wolf moon, umami Kickstarter artisan meggings cardigan drinking vinegar bicycle rights.</p>
+    </div>
+  </article>
+</section>
+<section class="panel b-green" id="4">
+  <article class="panel__wrapper">
+    <div class="panel__content">
+      <h1 class="panel__headline"><i class="fa fa-paper-plane"></i>&nbsp;Paper Planes</h1>
+      <div class="panel__block"></div>
+      <p>90's wayfarers lomo you probably haven't heard of them trust fund banh mi. Flannel Shoreditch dreamcatcher, quinoa flexitarian Banksy pickled post-ironic lo-fi. Photo booth asymmetrical tousled letterpress.</p>
+    </div>
+  </article>
+</section>
 			
 			
 		</div>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js">
 		
-		
+		<script src="index.js"></script>
 		
 		
 	</body>
