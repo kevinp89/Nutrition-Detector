fb7746ad88bcddfd872548070457d1b99db3f895
kevinp89 <kevinptl4@gmail.com>
2016-01-23 08:36:58 -0500

    KEVIN: uploading text files

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		
+		<title> Calorie Detector </title>
+		<link rel="stylesheet" type="text/css" href="main.css"> 
+		
+	</head>
+	<body>
+		<div class="nav-container">
+			<nav>
+				<ul id="nav-lst">
+					<li><a href="index.php"><span>Home</span></a></li>
+					<li><a href="#"><span>Contact Us</span></a></li>			
+				</ul>
+				
+				
+				
+			</nav>
+			
+			
+		</div>
+		
+		
+		
+		
+	</body>
+	
+	
+</html>
